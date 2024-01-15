6971ba114923a545e2da65bc881b9f347a0c3abf
MahdiT54 <mahditanzim2@gmail.com>
2024-01-15 08:57:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import v2logo from "./assets/v2-logo-2.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <header>
+        <a href="/">
+          <figure className="v2__logo">
+            <img className="v2__logo--img" src={v2logo} alt="" />
+          </figure>
         </a>
       </header>
-    </div>
+      <main>
+        <form id="login">
+          <div className="login-input">
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className="login-input">
+            <input type="text" placeholder="Password" />
+          </div>
+          <div className="login-submit">
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </main>
+      <footer>
+        <div className="footer__wrapper">
+          <div className="footer__text">© 2024 V2 Logistics Corp.</div>
+        </div>
+      </footer>
+    </>
   );
 }
 
